17da3987ebfe4d5fc402d529ccd6fdf46b626b07
Craig Duncan <git@duncanc.co.uk>
2014-06-03 13:55:44 +0100

    Created a cache class to provide a simple non-persistent memory cache

---
+++ src/Cache.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace duncan3dc\Helpers;
+
+class Cache {
+
+    private static $data = [];
+
+
+    public static function check($key) {
+
+        if(array_key_exists($key,static::$data)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+    public static function get($key) {
+
+        return static::$data[$key];
+
+    }
+
+
+    public static function set($key,$val) {
+
+        static::$data[$key] = $val;
+
+        return true;
+
+    }
+
+
+    public static function clear($key=false) {
+
+        if($key) {
+            unset(static::$data[$key]);
+
+        } else {
+            static::$data = [];
+
+        }
+
+        return true;
+
+    }
+
+
+    public function call($key,$func) {
+
+        $trace = debug_backtrace();
+        if($function = $trace[1]["function"]) {
+            $key = $function . "::" . $key;
+            if($class = $trace[1]["class"]) {
+                $key = $class . "::" . $key;
+            }
+        }
+
+        if(static::check($key)) {
+            return static::get($key);
+        }
+
+        $return = $func();
+
+        static::set($key,$return);
+
+        return $return;
+
+    }
+
+
+}
