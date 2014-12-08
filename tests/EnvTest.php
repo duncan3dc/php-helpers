duncan3dc/php-helpers

commit 61cd792793679535198a4d95bfbfb329643fcf38

Throw more relevant exceptions

src/Env.php
@@ -9,17 +9,17 @@ class Env
     /**
      * For use with usePath() - Represents the apache document root
      */
-    const PATH_DOCUMENT_ROOT    =   701;
+    const PATH_DOCUMENT_ROOT = 701;
 
     /**
      * For use with usePath() - Represents the directory that the PHP_SELF filename is in
      */
-    const PATH_PHP_SELF         =   702;
+    const PATH_PHP_SELF = 702;
 
     /**
      * For use with usePath() - Represents the parent of the vendor directory (commonly the project root)
      */
-    const PATH_VENDOR_PARENT    =   703;
+    const PATH_VENDOR_PARENT = 703;
 
     /**
      * @var string $path The root path to use
@@ -43,8 +43,8 @@ class Env
     {
         # Use the document root normally set via apache
         if ($path === self::PATH_DOCUMENT_ROOT) {
-            if (empty($_SERVER["DOCUMENT_ROOT"]) || !$path = realpath($_SERVER["DOCUMENT_ROOT"])) {
-                throw new \Exception("DOCUMENT_ROOT not defined");
+            if (empty($_SERVER["DOCUMENT_ROOT"]) || !is_dir($_SERVER["DOCUMENT_ROOT"])) {
+                throw new \InvalidArgumentException("DOCUMENT_ROOT not defined");
             }
             static::$path = $path;
             return;
@@ -53,7 +53,7 @@ class Env
         # Get the full path of the running script and use it's directory
         if ($path === self::PATH_PHP_SELF) {
             if (empty($_SERVER["PHP_SELF"]) || !$path = realpath($_SERVER["PHP_SELF"])) {
-                throw new \Exception("PHP_SELF not defined");
+                throw new \InvalidArgumentException("PHP_SELF not defined");
             }
             static::$path = pathinfo($path, PATHINFO_DIRNAME);
             return;
@@ -68,7 +68,7 @@ class Env
         if (is_dir($path)) {
             static::$path = $path;
         } else {
-            throw new \Exception("Invalid path specified");
+            throw new \InvalidArgumentException("Invalid path specified");
         }
     }
 

commit 641b26d045ae9de10beace1e7e593e03e8aeba05

Fixed a bug introduced in 61cd792793679535198a4d95bfbfb329643fcf38
Also added some tests to handle similar issues in future

src/Env.php
@@ -46,7 +46,7 @@ class Env
             if (empty($_SERVER["DOCUMENT_ROOT"]) || !is_dir($_SERVER["DOCUMENT_ROOT"])) {
                 throw new \InvalidArgumentException("DOCUMENT_ROOT not defined");
             }
-            static::$path = $path;
+            static::$path = $_SERVER["DOCUMENT_ROOT"];
             return;
         }
 

tests/EnvTest.php
@@ -8,7 +8,7 @@ class EnvTest extends \PHPUnit_Framework_TestCase
 {
     protected $path;
 
-    public function __construct()
+    public function setUp()
     {
         Env::usePath(Env::PATH_PHP_SELF);
         $this->path = realpath(pathinfo($_SERVER["PHP_SELF"], PATHINFO_DIRNAME));
@@ -19,6 +19,20 @@ class EnvTest extends \PHPUnit_Framework_TestCase
             "test-boolean"  =>  true,
             "test-exists"   =>  false,
         ]);
+    }
+
+    public function testDocumentRoot1()
+    {
+        unset($_SERVER["DOCUMENT_ROOT"]);
+        $this->setExpectedException("InvalidArgumentException");
+        Env::usePath(Env::PATH_DOCUMENT_ROOT);
+    }
+
+    public function testDocumentRoot2()
+    {
+        $_SERVER["DOCUMENT_ROOT"] = "/tmp";
+        Env::usePath(Env::PATH_DOCUMENT_ROOT);
+        $this->assertSame($_SERVER["DOCUMENT_ROOT"], Env::getPath());
     }
 
     public function testGetPath()