abbcdfbfc88fcde7164dd9f881b33c67e042f589
Craig Duncan <git@duncanc.co.uk>
2014-12-11 22:12:05 +0000

    Added tests for the Csv class

---
+++ tests/CsvTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace duncan3dc\Helpers;
+
+class CsvTest extends \PHPUnit_Framework_TestCase
+{
+    private $path;
+    private $file;
+    private $data;
+
+    public function setUp()
+    {
+        error_reporting(E_ALL);
+        if (!$this->path) {
+            $this->path = __DIR__ . "/files/file1.csv";
+            $this->file = new Csv($this->path);
+            $this->data = [
+                ["field1a", "field2a", "field3a"],
+                ["field1b", "field2b", "field3b"],
+            ];
+        }
+    }
+
+
+    public function testPutGet()
+    {
+        $this->assertSame($this->data, $this->file->put($this->data)->get());
+    }
+
+
+    public function testAppend()
+    {
+        $data = ["field1c", "field2c", "field3c"];
+        $check = $this->data;
+        $check[] = $data;
+        $this->assertSame($check, $this->file->append([$data])->get());
+    }
+
+
+    public function testStaticPut()
+    {
+        $path = tempnam("/tmp", "phpunit_csv_");
+
+        Csv::putContents($path, [$this->data[0]]);
+
+        $this->assertSame("field1a,field2a,field3a\n", file_get_contents($path));
+        unlink($path);
+    }
+
+
+    public function testStaticGet()
+    {
+        $path = tempnam("/tmp", "phpunit_csv_");
+
+        $file = fopen($path, "w");
+        foreach ($this->data as $row) {
+            fputcsv($file, $row);
+        }
+        fclose($file);
+
+        $this->assertSame($this->data, Csv::getContents($path));
+        unlink($path);
+    }
+
+
+    public function testArrayToString1()
+    {
+        $this->assertSame("\"ok ok\",ok\n", Csv::arrayToString([["ok ok", "ok"]]));
+    }
+
+
+    public function testArrayToString2()
+    {
+        $this->assertSame("\"ok \"\"ok\",ok\n", Csv::arrayToString([["ok \"ok", "ok"]]));
+    }
+
+
+    public function testArrayToString3()
+    {
+        $this->assertSame("\"secret,comma\",ok\n", Csv::arrayToString([["secret,comma", "ok"]]));
+    }
+
+
+    private function expectRuntimeException()
+    {
+        error_reporting(\E_ALL ^ \E_WARNING);
+        $this->setExpectedException("RuntimeException");
+    }
+
+
+    public function testStaticPutFail()
+    {
+        $this->expectRuntimeException();
+        Csv::putContents($this->path . "/does-not-exist", []);
+    }
+
+
+    public function testStaticGetFail()
+    {
+        $this->expectRuntimeException();
+        Csv::getContents($this->path . "/does-not-exist", []);
+    }
+
+
+    public function testStaticAppendFail()
+    {
+        $this->expectRuntimeException();
+        Csv::appendContents($this->path . "/does-not-exist", []);
+    }
+}
